f08cc7d54b8283d4046e9e87f2306c6f1f5494c4
Logan Praznik <lpraz@mts.net>
2019-12-02 21:45:31 -0600

    Rewrote game state to be more flexible

--- src/Game.hs
+++ src/Game.hs
@@ -3,46 +3,72 @@ module Game
     , initState
     ) where
 
-data GameState = Invalid
-    | GameState
-    { room :: Room }
+import Data.List (find)
 
-data Room = LivingRoom | Bathroom | Bedroom
+data GameState = GameState
+    { rooms :: [Room] }
 
-isValid :: GameState -> Bool
-isValid Invalid = False
-isValid _ = True
+type RoomKey = String
+
+data Room = Room
+    { roomKey :: RoomKey
+    , roomDescription :: String
+    , roomLinks :: [RoomKey]
+    , roomEntities :: [Entity] }
+
+data Entity = Player
+    deriving (Eq)
+
+findPlayer :: [Room] -> Maybe Room
+findPlayer = find (\x -> Player `elem ` roomEntities x)
 
 describe :: Room -> String
-describe LivingRoom = "You are in the living room."
-describe Bathroom = "You are in the bathroom."
-describe Bedroom = "You are in the bedroom."
+describe = roomDescription
 
-goto :: String -> GameState
-goto "livingroom" = GameState LivingRoom
-goto "bathroom" = GameState Bathroom
-goto "bedroom" = GameState Bedroom
-goto _ = Invalid
+livingRoom :: Room
+livingRoom = Room
+    "livingroom"
+    "A living room/kitchen. It looks mildly festive."
+    ["bathroom", "bedroom"]
+    [Player]
+
+bathroom :: Room
+bathroom = Room
+    "bathroom"
+    "A large bathroom. The walls are painted white."
+    ["livingroom"]
+    []
+
+bedroom :: Room
+bedroom = Room
+    "bedroom"
+    "A sizeable bedroom. There is a bed with several blankets."
+    ["livingroom"]
+    []
 
 initState :: GameState
-initState = GameState LivingRoom
+initState = GameState rooms
+    where
+    rooms = [livingRoom, bathroom, bedroom]    
 
 -- Game loop. Recursive: we only actually return the IO () when user exits
 run :: GameState -> IO ()
 run state = do
     -- Output
-    putStrLn . describe . room $ state
-    -- Get input
-    input <- getLine
-    let command = words input
-    -- Change state
-    case head command of
-        "quit" -> putStrLn "See you later!"
-        "goto" -> if length command < 2 then invalid else do
-            let newState = goto (command !! 1)
-            if isValid newState then run newState else invalid
-        _ -> invalid
-    where
-        invalid = do
-            putStrLn "Didn't understand that command."
-            run state
+    let maybeRoom = findPlayer . rooms $ state
+    case maybeRoom of
+        Nothing -> error "Player not in a room"
+        Just room -> do
+            putStrLn . describe $ room
+            -- Get input
+            input <- getLine
+            let command = words input
+            -- Change state
+            case head command of
+                "quit" -> putStrLn "See you later!"
+                -- TODO: goto
+                _ -> invalid
+            where
+                invalid = do
+                    putStrLn "Didn't understand that command."
+                    run state
