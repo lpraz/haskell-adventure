648ce0031c9e67bd8e132564e61300a445c3d0fd
Logan Praznik <lpraz@mts.net>
2019-12-03 21:04:49 -0600

    Re-implemented navigation (goto)

--- src/Game.hs
+++ src/Game.hs
@@ -3,10 +3,12 @@ module Game
     , initState
     ) where
 
-import Data.List (find)
+import Data.List (find, intercalate)
 
 data GameState = GameState
-    { rooms :: [Room] }
+    { rooms :: [Room]
+    , location :: RoomKey
+    }
 
 type RoomKey = String
 
@@ -14,59 +16,71 @@ data Room = Room
     { roomKey :: RoomKey
     , roomDescription :: String
     , roomLinks :: [RoomKey]
-    , roomEntities :: [Entity] }
-
-data Entity = Player
-    deriving (Eq)
-
-findPlayer :: [Room] -> Maybe Room
-findPlayer = find (\x -> Player `elem ` roomEntities x)
-
-describe :: Room -> String
-describe = roomDescription
+    }
 
 livingRoom :: Room
 livingRoom = Room
     "livingroom"
     "A living room/kitchen. It looks mildly festive."
     ["bathroom", "bedroom"]
-    [Player]
 
 bathroom :: Room
 bathroom = Room
     "bathroom"
     "A large bathroom. The walls are painted white."
     ["livingroom"]
-    []
 
 bedroom :: Room
 bedroom = Room
     "bedroom"
     "A sizeable bedroom. There is a bed with several blankets."
     ["livingroom"]
-    []
 
 initState :: GameState
-initState = GameState rooms
-    where
-    rooms = [livingRoom, bathroom, bedroom]    
+initState = GameState rooms (roomKey livingRoom)
+    where rooms = [livingRoom, bathroom, bedroom]  
+    
+findRoom :: RoomKey -> [Room] -> Maybe Room
+findRoom key = find (\x -> key == roomKey x)
+
+goto :: RoomKey -> GameState -> Maybe GameState
+goto key state =
+    case findRoom (location state) (rooms state) of
+        Nothing -> Nothing
+        Just room -> 
+            if key `elem` roomLinks room
+                then Just $ GameState (rooms state) key
+                else Nothing
+
+-- TODO: should we really be throwing error?
+describe :: GameState -> String
+describe state = case findRoom (location state) (rooms state) of
+    Nothing -> error "Invalid player location"
+    Just room -> unlines
+        [ ""
+        , roomDescription room
+        , "From here, you can go to: " ++ intercalate ", " (roomLinks room)
+        ]
 
 -- Game loop. Recursive: we only actually return the IO () when user exits
 run :: GameState -> IO ()
 run state = do
     -- Output
-    let maybeRoom = findPlayer . rooms $ state
+    let maybeRoom = findRoom (location state) (rooms state)
     case maybeRoom of
-        Nothing -> error "Player not in a room"
+        Nothing -> error "Invalid player location"
         Just room -> do
-            putStrLn . describe $ room
+            putStrLn (describe state)
+            putStrLn "What would you like to do?"
             -- Get input
             input <- getLine
             let command = words input
             -- Change state
             case head command of
                 "quit" -> putStrLn "See you later!"
-                -- TODO: goto
+                "goto" -> case goto (command !! 1) state of
+                    Just newState -> run newState
+                    Nothing -> invalid
                 _ -> invalid
             where
                 invalid = do
