b143447dd9f26eebfa19cc960f9e4427158d51e0
lpraz <lpraz@mts.net>
2019-12-08 18:10:56 -0600

    Added items, 'take' command

--- src/Game.hs
+++ src/Game.hs
@@ -8,7 +8,9 @@ import Data.List (find, intercalate)
 data GameState = GameState
     { rooms :: [Room]
     , location :: RoomKey
+    , inventory :: [Item]
     }
+    deriving (Show)
 
 type RoomKey = String
 
@@ -16,32 +18,51 @@ data Room = Room
     { roomKey :: RoomKey
     , roomDescription :: String
     , roomLinks :: [RoomKey]
+    , roomItems :: [Item]
     }
+    deriving (Show)
+
+type ItemKey = String
+
+data Item = Item
+    { itemKey :: ItemKey
+    , itemDescription :: String
+    }
+    deriving (Show)
+
+spork :: Item
+spork = Item "spork" "a plastic spork"
 
 livingRoom :: Room
 livingRoom = Room
     "livingroom"
     "A living room/kitchen. It looks mildly festive."
     ["bathroom", "bedroom"]
+    [spork]
 
 bathroom :: Room
 bathroom = Room
     "bathroom"
     "A large bathroom. The walls are painted white."
     ["livingroom"]
+    []
 
 bedroom :: Room
 bedroom = Room
     "bedroom"
     "A sizeable bedroom. There is a bed with several blankets."
     ["livingroom"]
+    []
 
 initState :: GameState
-initState = GameState rooms (roomKey livingRoom)
+initState = GameState rooms (roomKey livingRoom) []
     where rooms = [livingRoom, bathroom, bedroom]  
     
 findRoom :: RoomKey -> [Room] -> Maybe Room
 findRoom key = find (\x -> key == roomKey x)
+
+findItem :: ItemKey -> [Item] -> Maybe Item
+findItem key = find (\x -> key == itemKey x)
 
 goto :: RoomKey -> GameState -> Maybe GameState
 goto key state =
@@ -49,8 +70,24 @@ goto key state =
         Nothing -> Nothing
         Just room -> 
             if key `elem` roomLinks room
-                then Just $ GameState (rooms state) key
+                then Just $ GameState (rooms state) key (inventory state)
                 else Nothing
+
+take :: ItemKey -> GameState -> Maybe GameState
+take key state =
+    let room = findRoom (location state) (rooms state) in
+    case room of
+        Nothing -> Nothing
+        Just room ->
+            let item = findItem key (roomItems room) in
+            case item of
+                Nothing -> Nothing
+                Just item -> 
+                    let newRoomItems = filter (\i -> itemKey i /= key) (roomItems room)
+                        newRoom = Room (roomKey room) (roomDescription room) (roomLinks room) newRoomItems
+                        newRooms = newRoom : filter (\r -> roomKey r /= roomKey room) (rooms state)
+                        newInventory = item : inventory state in
+                    Just $ GameState newRooms (location state) newInventory
 
 -- TODO: should we really be throwing error?
 describe :: GameState -> String
@@ -59,7 +96,9 @@ describe state = case findRoom (location state) (rooms state) of
     Just room -> unlines
         [ ""
         , roomDescription room
+        , "Here, there is: " ++ intercalate ", " (map itemDescription (roomItems room))
         , "From here, you can go to: " ++ intercalate ", " (roomLinks room)
+        , "You are holding: " ++ intercalate ", " (map itemDescription (inventory state))
         ]
 
 -- Game loop. Recursive: we only actually return the IO () when user exits
@@ -81,8 +120,11 @@ run state = do
                 "goto" -> case goto (command !! 1) state of
                     Just newState -> run newState
                     Nothing -> invalid
+                "take" -> case Game.take (command !! 1) state of
+                    Just newState -> run newState
+                    Nothing -> invalid
                 _ -> invalid
-            where
-                invalid = do
-                    putStrLn "Didn't understand that command."
-                    run state
+    where
+        invalid = do
+            putStrLn "Didn't understand that command."
+            run state
